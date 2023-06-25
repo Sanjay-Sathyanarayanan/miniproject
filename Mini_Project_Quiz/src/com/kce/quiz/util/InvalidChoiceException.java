534a32076ecd371ceba12cf3fd6fc0f54c25cbb9
Sanjay Sathyanarayanan <97724234+Sanjay-Sathyanarayanan@users.noreply.github.com>
2023-06-25 22:17:09 +0530

    Delete Mini_Project_Quiz directory

--- Mini_Project_Quiz/src/com/kce/quiz/util/InvalidChoiceException.java
+++
@@ -1,8 +0,0 @@
-package com.kce.quiz.util;
-
-public class InvalidChoiceException extends Exception{
- @Override
-	public String toString() {
-	return "You have Selected wrong option ! select from the above choice a - d";
-	}
-}
--- Mini_Project_Quiz/src/com/kce/quiz/util/InvalidQuizIDException.java
+++
@@ -1,6 +0,0 @@
-package com.kce.quiz.util;
-public class InvalidQuizIDException extends Exception{
-       public String toString() {
-    	   return "You have selected the wrong Quiz Id \n!!! Please select from the above list !!!";
-       }
-}
